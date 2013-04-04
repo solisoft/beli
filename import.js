f52370b91befb37de54ec949d0ab653b4fb82f9c
Olivier Bonnaure <o.bonnaure@solisoft.net>
2013-04-04 17:04:10 +0200

    import

--- import.js
+++ import.js
@@ -36,8 +36,9 @@ if(video == "") {
   for (var i = 0; i < imgs.length; ++i) {
     var item = imgs[i];
     var src = item.getAttribute("src");
-    var text = item.getAttribute("title").replace("/","-");
+    var text = item.getAttribute("title");
     if(text == null) text = "";
+    text = text.replace("/","-");
     if(src && src.indexOf("http") == -1) src = document.location.origin +"/"+ src;
     if(item.width > 100 && item.height > 100 && y < max) {
       html += "<div style='display:inline-block; margin: 10px;width:150px; height: 150px; overflow: hidden;'><a href='http://dev.24s46.com/import/"+encodeURIComponent(src)+"/"+encodeURIComponent(document.location.href)+"/"+encodeURIComponent(text)+"' alt='"+text.replace("'", "\\'")+"' title='"+text.replace("'", "\\'")+"' target='beli'><img src='" + src + "' width='150'></a></div>";
