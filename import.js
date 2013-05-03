e47c73ee1aa51a65638b1cdef1aee1e8dff4ec85
Olivier Bonnaure <o.bonnaure@solisoft.net>
2013-05-03 17:29:14 +0200

    import

--- import.js
+++ import.js
@@ -19,25 +19,19 @@ video = '';
 // Vimeo
 if(document.location.href.match(/vimeo.com\/(\d+)/)) {
   var id = document.location.href.match(/vimeo.com\/(\d+)/)[1];
-  video = '<iframe src="http://player.vimeo.com/video/'+id+'"
-width="300" height="150" frameborder="0" webkitAllowFullScreen
-mozallowfullscreen allowFullScreen></iframe> <p><a href="http://www.24s46.com/import_video?engine=vimeo&id='+id+'&url='+encodeURIComponent(document.location.href)+'"  target="beli">Importer</a></p>';
+  video = '<iframe src="http://player.vimeo.com/video/'+id+'" width="300" height="150" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe> <p><a href="http://www.24s46.com/import_video?engine=vimeo&id='+id+'&url='+encodeURIComponent(document.location.href)+'"  target="beli">Importer</a></p>';
   html += video;
 }
 // Youtube
 if(document.location.href.match(/www.youtube.com\/watch\?v=([\d\w]+)/)) {
   var id = document.location.href.match(/www.youtube.com\/watch\?v=([\d\w]+)/)[1];
-  video = '<iframe width="300" height="150"
-src="http://www.youtube.com/embed/'+id+'" frameborder="0"
-allowfullscreen></iframe> <p><a href="http://www.24s46.com/import_video?engine=youtube&id='+id+'&url='+encodeURIComponent(document.location.href)+'"  target="beli">Importer</a></p>';
+  video = '<iframe width="300" height="150" src="http://www.youtube.com/embed/'+id+'" frameborder="0" allowfullscreen></iframe> <p><a href="http://www.24s46.com/import_video?engine=youtube&id='+id+'&url='+encodeURIComponent(document.location.href)+'"  target="beli">Importer</a></p>';
   html += video;
 }
 // DailyMotion
 if(document.location.href.match(/\/\/www.dailymotion.com\/video\/([\d\w]+)_/)) {
   var id = document.location.href.match(/\/\/www.dailymotion.com\/video\/([\d\w]+)_/)[1];
-  video = '<iframe frameborder="0" width="480" height="270"
-src="http://www.dailymotion.com/embed/video/'+id+'"></iframe> <p><a
-href="http://www.24s46.com/import_video?engine=dailymotion&id='+id+'&url='+encodeURIComponent(document.location.href)+'"  target="beli">Importer</a></p>';
+  video = '<iframe frameborder="0" width="480" height="270" src="http://www.dailymotion.com/embed/video/'+id+'"></iframe> <p><a href="http://www.24s46.com/import_video?engine=dailymotion&id='+id+'&url='+encodeURIComponent(document.location.href)+'"  target="beli">Importer</a></p>';
   html += video;
 }
 
@@ -56,8 +50,7 @@ if(video == "") {
     if(src && src.indexOf("http") == -1) src = document.location.origin +"/"+ src;
     if(src && src.indexOf("data:image") == -1) {
       if(item.width >= 214 && item.height >= 100 && y < max) {
-        html += "<div style='float:left; margin: 10px;width:150px;
-height: 90px; overflow: hidden;'><a href='http://www.24s46.com/import/"+encodeURIComponent(src)+"/"+encodeURIComponent(document.location.href)+"/"+encodeURIComponent(text)+"' alt='"+text.replace(/'/g, "\\'")+"' title='"+text.replace(/'/g, "\\'")+"' target='beli'><img src='" + src + "' width='150' style='box-shadow: 0 0 10px #000;border-radius: 4px;'></a></div>";
+        html += "<div style='float:left; margin: 10px;width:150px; height: 90px; overflow: hidden;'><a href='http://www.24s46.com/import/"+encodeURIComponent(src)+"/"+encodeURIComponent(document.location.href)+"/"+encodeURIComponent(text)+"' alt='"+text.replace(/'/g, "\\'")+"' title='"+text.replace(/'/g, "\\'")+"' target='beli'><img src='" + src + "' width='150' style='box-shadow: 0 0 10px #000;border-radius: 4px;'></a></div>";
         y++;
       }
     }
